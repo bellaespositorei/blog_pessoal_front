d6ce73b64308f8e5a4a23d753552b4eeefe2fff6
bellaespositorei <contato.isabellaesposito@gmail.com>
2024-08-12 10:07:24 -0300

    :sparkles: feat: Crie página de login, cadastro e Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
 import React from "react";
 import "./App.css";
-import Home from "./pages/Home/Home";
+
 import Navbar from "./components/navBar/NavBar";
 import Footer from "./components/Footer/Footer";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login/Login";
+import Cadastro from "./pages/cadastro/Cadastro";
+import Home from "./pages/Home/Home";
+import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Navbar />
-        <div className="min-h-[80vh]">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/home" element={<Home />} />
-          </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+          <div className="min-h-[80vh]">
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </>
   );
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import profile from "../../assets/profile.jpeg";
+import homeLogo from "../../assets/home.png";
 import "./Home.css";
 
 function Home() {
@@ -19,7 +19,7 @@ function Home() {
           </div>
 
           <div className="flex justify-center ">
-            <img src={profile} alt="" className="w-2/3" />
+            <img src={homeLogo} alt="" className="w-2/3" />
           </div>
         </div>
       </div>
